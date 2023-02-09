1863a49c29423ac447efc03f3038f8494a673535
李杰 <827188236@qq.com>
2023-02-09 11:38:25 +0800

    commit

--- components/Create.tsx
+++ components/Create.tsx
@@ -3,6 +3,10 @@ import Rmodynamics from './Rmodynamics'
 import DashRmodynamics from './DashRmodynamics'
 import DashLinemics from './DashLinemics'
 import api from "../api";
+import Image from 'next/image'
+import IconUp from '../statics/img/up.svg'
+import IconDown from '../statics/img/down.svg'
+
 import { LeftOutlined, RightOutlined, LoadingOutlined } from '@ant-design/icons';
 
 const Create = () => {
@@ -14,8 +18,8 @@ const Create = () => {
 
     const getScores = async () => {
         setLoadingScores(true)
-        const res = await api.get(`/publication/score/${testProfileId}`,{
-            params:{
+        const res = await api.get(`/publication/score/${testProfileId}`, {
+            params: {
                 profileId: testProfileId
             }
         })
@@ -23,44 +27,91 @@ const Create = () => {
         setLoadingScores(false)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getScores();
     }, [])
     return (
         <>
             <div className="absolute top-[20px] text-[24px]">Create</div>
             <div className="h-[calc(100vh-60px)] overflow-y-auto">
-            {/* <PostEdit></PostEdit> */}
-            <div className="flex mb-10 mt-[60px]">
-                {(loadingScores || !scores.now)? <LoadingOutlined className="text-2xl block mx-auto my-4" /> : <>
-                    <div className="w-[300px] p-4 bg-[#1A1A1A] text-[rgba(255,255,255,0.5)] mr-10">
-                        <div className="mb-2">Creator</div>
-                        <div className="mb-1 text-[24px] text-[rgba(255,255,255,0.9)]">#{scores.now.pr_rank_creator}</div>
-                        <div>
-                            {scores.lastWeek.pr_rank_creator - scores.now.pr_rank_creator} this week
-                            
+                {/* <PostEdit></PostEdit> */}
+                <div className="flex mb-10 mt-[60px]">
+                    {(loadingScores || !scores.now) ? <LoadingOutlined className="text-2xl block mx-auto my-4" /> : <>
+                        <div className="w-[300px] p-4 bg-[#1A1A1A] text-[rgba(255,255,255,0.5)] mr-10">
+                            <div className="mb-2">Creator</div>
+                            <div className="mb-1 text-[24px] text-[rgba(255,255,255,0.9)]">#{scores.now.pr_rank_creator}</div>
+                            <div className="flex items-center">
+                                {
+                                    scores.lastWeek.pr_rank_creator - scores.now.pr_rank_creator < 0 &&
+                                    <Image
+                                        className="mr-1"
+                                        src={IconDown}
+                                        alt=""
+                                    />
+                                }
+                                {
+                                    scores.lastWeek.pr_rank_creator - scores.now.pr_rank_creator > 0 &&
+                                    <Image
+                                        className="mr-1"
+                                        src={IconUp}
+                                        alt=""
+                                    />
+                                }
+                                <span>{scores.lastWeek.pr_rank_creator - scores.now.pr_rank_creator} this week</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="w-[300px] p-4 bg-[#1A1A1A] text-[rgba(255,255,255,0.5)] mr-10">
-                        <div className="mb-2">Engager</div>
-                        <div className="mb-1 text-[24px] text-[rgba(255,255,255,0.9)]">#{scores.now.pr_rank_engager}</div>
-                        <div>{scores.lastWeek.pr_rank_engager - scores.now.pr_rank_engager} this week</div>
-                    </div>
-                    <div className="w-[300px] p-4 bg-[#1A1A1A] text-[rgba(255,255,255,0.5)] mr-10">
-                        <div className="mb-2">Campaign</div>
-                        <div className="mb-1 text-[24px] text-[rgba(255,255,255,0.9)]">#{scores.now.pr_rank_compaign}</div>
-                        <div>{scores.lastWeek.pr_rank_compaign - scores.now.pr_rank_compaign} this week</div>
-                    </div>
-                </>}
+                        <div className="w-[300px] p-4 bg-[#1A1A1A] text-[rgba(255,255,255,0.5)] mr-10">
+                            <div className="mb-2">Engager</div>
+                            <div className="mb-1 text-[24px] text-[rgba(255,255,255,0.9)]">#{scores.now.pr_rank_engager}</div>
+                            <div className="flex items-center">
+                                {
+                                    scores.lastWeek.pr_rank_engager - scores.now.pr_rank_engager < 0 &&
+                                    <Image
+                                        className="mr-1"
+                                        src={IconDown}
+                                        alt=""
+                                    />
+                                }
+                                {
+                                    scores.lastWeek.pr_rank_engager - scores.now.pr_rank_engager > 0 &&
+                                    <Image
+                                        className="mr-1"
+                                        src={IconUp}
+                                        alt=""
+                                    />
+                                }
+                                <span>{scores.lastWeek.pr_rank_engager - scores.now.pr_rank_engager} this week</span>
+                            </div>
+                        </div>
+                        <div className="w-[300px] p-4 bg-[#1A1A1A] text-[rgba(255,255,255,0.5)] mr-10">
+                            <div className="mb-2">Campaign</div>
+                            <div className="mb-1 text-[24px] text-[rgba(255,255,255,0.9)]">#{scores.now.pr_rank_compaign}</div>
+                            <div className="flex items-center">
+                                {
+                                    scores.lastWeek.pr_rank_compaign - scores.now.pr_rank_compaign < 0 &&
+                                    <Image
+                                        className="mr-1"
+                                        src={IconDown}
+                                        alt=""
+                                    />
+                                }
+                                {
+                                    scores.lastWeek.pr_rank_compaign - scores.now.pr_rank_compaign > 0 &&
+                                    <Image
+                                        className="mr-1"
+                                        src={IconUp}
+                                        alt=""
+                                    />
+                                }
+                                <span>{scores.lastWeek.pr_rank_compaign - scores.now.pr_rank_compaign} this week</span>
+                            </div>
+                        </div>
+                    </>}
+                </div>
+                <Rmodynamics></Rmodynamics>
+                <DashRmodynamics></DashRmodynamics>
+                <DashLinemics></DashLinemics>
             </div>
-            <Rmodynamics></Rmodynamics>
-            {/* <div className="mt-10 text-[18px] mb-4">Best days and times to publish</div>
-            <div className="text-[14px] text-[rgba(255,255,255,0.5)] mb-10">Base on your number of followers online in the last 30 days, we suggest publish on:</div>
-            <BestCard></BestCard> */}
-            
-            <DashRmodynamics></DashRmodynamics>
-            <DashLinemics></DashLinemics>
-        </div>
         </>
     )
 }
