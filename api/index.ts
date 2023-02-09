d5ea833d0379af34c65e61c7d0d16745d9413843
李杰 <827188236@qq.com>
2023-02-09 11:39:03 +0800

    commit

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,7 @@ const api = axios.create({
   baseURL,
 });
 
-const knn3Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyZXNzIjoiMHgwYThlYTRkOGUzZTA2OGM2YjZhNDM1OGQ2ZjA2OGJiMDRhZDA0ZGRmIiwicHJvZmlsZUlkcyI6WzQ3MTMwLDU0NjEzLDkxNjEzLDQ3MTI3LDQ3MTA3XSwibGVuc1Byb2ZpbGVORlQiOnRydWUsImxlbnNSYWluYm93TkZUIjp0cnVlLCJpYXQiOjE2NzU5MDc3NTIsImV4cCI6MTY3NTkyNTc1Mn0.suDgxvAis62G-BlXWtnydmbCv5TdAawLGR9aSLl1t2U'
+const knn3Token = typeof window !== 'undefined' && sessionStorage.getItem('knn3Token')
 
 if(knn3Token){
   api.defaults.headers.authorization = `Bearer ${knn3Token}`
