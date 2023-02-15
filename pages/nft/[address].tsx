a9a4f3c47abaa84cf4f9e16ef38420bfed79e308
rocker <exrobbie@gmail.com>
2023-02-15 10:39:58 +0800

    updated og

--- pages/nft/[address].tsx
+++ pages/nft/[address].tsx
@@ -153,7 +153,7 @@ const nft = ({
         <meta name="twitter:title" content="Unlock your web3 social presence with TopScore！" />
         <meta
           name="twitter:description"
-          content="TopScore is a user-friendly tool that enables users to create,build, and earn their social presence."
+          content="TopScore - a user-friendly tool that enables users to create, build, and earn their social presence."
         />
         <meta
           property="twitter:image"
@@ -162,7 +162,7 @@ const nft = ({
         <meta property="og:title" content="Unlock your web3 social presence with TopScore！" />
         <meta
           property="og:description"
-          content="TopScore is a user-friendly tool that enables users to create,build, and earn their social presence."
+          content="TopScore - a user-friendly tool that enables users to create, build, and earn their social presence."
         />
         <meta
           property="og:image"
