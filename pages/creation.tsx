dace1cfee2c748a2b8e77f47420c13cdc7396603
李杰 <827188236@qq.com>
2023-04-07 09:42:47 +0800

    commit

--- pages/creation.tsx
+++ pages/creation.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from "react";
-import log from "../lib/log";
 import Navbar from "../components/Navbar";
 import useWeb3Context from "../hooks/useWeb3Context";
 import ConnectBtn from "../components/ConnectBtn";
@@ -7,10 +6,6 @@ import Create from "../components/Create";
 
 const Post = () => {
   const { account } = useWeb3Context();
-
-  useEffect(() => {
-    log("visit_create", account);
-  }, []);
 
   return (
     <div className="w-full h-full bg-[#000] flex">
