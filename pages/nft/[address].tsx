2b7ebd1d3357d512c2277670e87d138af6000e9d
李杰 <827188236@qq.com>
2023-04-19 17:18:29 +0800

    metamask error

--- pages/nft/[address].tsx
+++ pages/nft/[address].tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import api from "../../api";
+import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 import { Spin } from 'antd'
 import { LoadingOutlined } from "@ant-design/icons";
 import useLenshubContract from "../../contract/useLenshubContract";
-
+const errorMsg = `Metamask plugin not found or not active. Please check your browser's plugin list.`
 const nft = () => {
 
     const router = useRouter()
@@ -13,7 +14,12 @@ const nft = () => {
 
     const lenshubContract = useLenshubContract();
 
-    const getActiveProfile = async (address: any, profileId: any) => {
+    const getActiveProfile = async () => {
+        if (!window.ethereum) {
+            router.push(`/profile/stani`)
+            toast.info(errorMsg)
+            return
+        }
         const res: any = await lenshubContract.getHandle(router.query.profileId);
         console.log(res)
         if (res) {
@@ -25,7 +31,7 @@ const nft = () => {
 
     useEffect(() => {
         if (router.query.address && router.query.profileId) {
-            getActiveProfile(router.query.address, router.query.profileId)
+            getActiveProfile()
         }
     }, [router])
 
@@ -33,7 +39,7 @@ const nft = () => {
         <div className="w-full h-full bg-[#000] flex text-[#fff]">
             <div className="h-full w-full flex items-center justify-center">
                 <Spin indicator={<LoadingOutlined style={{ fontSize: 36 }} spin />} />
-              </div>
+            </div>
         </div>
     )
 }
