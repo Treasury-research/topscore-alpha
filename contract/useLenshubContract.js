45cd9b24d982b6daeeb7b6f978c88dc5dba64866
李杰 <827188236@qq.com>
2023-04-19 17:03:37 +0800

    modify

--- contract/useLenshubContract.js
+++ contract/useLenshubContract.js
@@ -3,6 +3,7 @@ import useErc721Contract from "./useErc721Contract";
 import useWeb3Context from "hooks/useWeb3Context";
 import useContract from "hooks/useContract";
 import LenshubAbi from "./abi/Lenshub.json";
+import Web3 from "web3";
 export default function useLenshubContract() {
   const { web3, sendTx, account } = useWeb3Context();
   const contract = useContract(LenshubAbi, config.contracts.lenshub);
@@ -11,6 +12,11 @@ export default function useLenshubContract() {
   return {
     async getProfile(profileId) {
       return await contract.methods.getProfile(profileId).call();
+    },
+    async getHandle(profileId) {
+      const lensWeb3 = new Web3(window.ethereum)
+      const lensContract = new lensWeb3.eth.Contract(LenshubAbi, config.contracts.lenshub);
+      return await lensContract.methods.getHandle(profileId).call();
     },
     async follow(profileId, feeInfo) {
       const initData = web3.eth.abi.encodeParameters(
--- pages/nft/[address].tsx
+++ pages/nft/[address].tsx
@@ -3,34 +3,34 @@ import api from "../../api";
 import { useRouter } from "next/router";
 import { Spin } from 'antd'
 import { LoadingOutlined } from "@ant-design/icons";
+import useLenshubContract from "../../contract/useLenshubContract";
 
 const nft = () => {
 
     const router = useRouter()
-    
+
     const [activeProfile, setActiveProfile] = useState<any>({});
 
+    const lenshubContract = useLenshubContract();
+
     const getActiveProfile = async (address: any, profileId: any) => {
-        const res: any = await api.get(`/lens/handles/${address}`);
-        console.log('res', res)
-        const filtered = res.data.filter((item: any) => item.profileId == profileId)[0]
-        if (filtered && filtered.handle) {
-            router.push(`/profile/${filtered.handle.split('.lens')[0]}`)
+        const res: any = await lenshubContract.getHandle(router.query.profileId);
+        console.log(res)
+        if (res) {
+            router.push(`/profile/${res.split('.lens')[0]}`)
         } else {
             router.push(`/profile/stani`)
         }
     }
 
     useEffect(() => {
-        console.log(router)
         if (router.query.address && router.query.profileId) {
-
             getActiveProfile(router.query.address, router.query.profileId)
         }
     }, [router])
 
     return (
-        <div className="w-full h-full bg-[#000] flex">
+        <div className="w-full h-full bg-[#000] flex text-[#fff]">
             <div className="h-full w-full flex items-center justify-center">
                 <Spin indicator={<LoadingOutlined style={{ fontSize: 36 }} spin />} />
               </div>
